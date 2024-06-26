b324e4ee51c4a2793cd67ac985334cf603bc44ae
eminokumus1 <eminokumus1@gmail.com>
2024-06-26 17:15:55 +0300

    MovieDetailsNetworkDataSource class fetches data from network with using Api Service. RxJava used for background threading.

---
+++ app/src/main/java/com/eminokumus/moviesapp/data/repository/MovieDetailsNetworkDataSource.kt
@@ -0,0 +1,42 @@
+package com.eminokumus.moviesapp.data.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.eminokumus.moviesapp.data.api.TheMovieDBInterface
+import com.eminokumus.moviesapp.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MovieDetailsNetworkDataSource(
+    private val apiService: TheMovieDBInterface,
+    private val compositeDisposable: CompositeDisposable
+) {
+    private val _networkState = MutableLiveData<NetworkState>()
+    val networkState: LiveData<NetworkState> get() = _networkState
+
+    private val _downloadedMovieDetailsResponse = MutableLiveData<MovieDetails>()
+    val downloadedMovieDetailsResponse: LiveData<MovieDetails> get() = _downloadedMovieDetailsResponse
+
+    fun fetchMovieDetails(movieId: Int){
+        _networkState.postValue(NetworkState.LOADING)
+        try {
+            compositeDisposable.add(
+                apiService.getMovieDetails(movieId)
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        {
+                            _downloadedMovieDetailsResponse.postValue(it)
+                            _networkState.postValue(NetworkState.LOADED)
+                        },
+                        {
+                            _networkState.postValue(NetworkState.ERROR)
+                            it.message?.let { it1 -> Log.e("MovieDetailsDataSource", it1) }
+                        }
+                    )
+            )
+        }catch (e: Exception){
+            e.message?.let { Log.e("MovieDetailsDataSource", it) }
+        }
+    }
+}
