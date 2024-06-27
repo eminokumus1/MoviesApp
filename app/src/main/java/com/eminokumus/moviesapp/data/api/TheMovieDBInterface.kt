ce1ce8e3f2e3401ba756da7c007e66053fa82d1b
eminokumus1 <eminokumus1@gmail.com>
2024-06-27 12:49:59 +0300

    method for getting popular movies from database added.

--- app/src/main/java/com/eminokumus/moviesapp/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/eminokumus/moviesapp/data/api/TheMovieDBInterface.kt
@@ -1,15 +1,20 @@
 package com.eminokumus.moviesapp.data.api
 
 import com.eminokumus.moviesapp.data.vo.MovieDetails
+import com.eminokumus.moviesapp.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 // https://api.themoviedb.org/3/movie/573435?api_key=666cceee3696eec9c1bc3765ca81e5d4
-// https://api.themoviedb.org/3/movie/popular?api_key=666cceee3696eec9c1bc3765ca81e5d4
+// https://api.themoviedb.org/3/movie/popular?api_key=666cceee3696eec9c1bc3765ca81e5d4&page=1
 
 
 interface TheMovieDBInterface {
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
+
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
 
